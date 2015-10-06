fd55dd9f124fe2d69447e6160db5d61642dca559
Francisco Cardoso <francisco@bitreserve.org>
2015-10-06 18:26:14 +0100

    Add ITIN validator

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
 # Individual Taxpayer Identification Numbers (ITIN)
+
+[![npm version][npm-image]][npm-url]
+[![build status][travis-image]][travis-url]
+
+This modules allows you to check if a number is a valid.
+
+## Installation
+
+Choose your preferred method:
+
+* npm: `npm install --save is-valid-itin`
+* Download: [is-valid-itin](https://github.com/seegno/is-valid-itin)
+
+## Usage
+
+*NOTE:* The input number **must not** be formatted to `xxx-xx-xxxx`.
+
+> Check if number is valid.
+
+```js
+import isValidItin from 'is-valid-itin';
+
+isValidItin('xxxxxxxxx');
+```
+
+> Mask the number.
+
+```js
+import { mask } from 'is-valid-itin';
+
+mask('xxxxxxxxx');
+```
+
+## Running tests
+
+```sh
+npm test
+```
+
+[npm-image]: https://img.shields.io/npm/v/is-valid-itin.svg?style=flat-square
+[npm-url]: https://npmjs.org/package/is-valid-itin
+[travis-image]: https://img.shields.io/travis/seegno/is-valid-itin.svg?style=flat-square
+[travis-url]: https://travis-ci.org/seegno/is-valid-itin
---
+++ test/index_test.js
@@ -0,0 +1,90 @@
+
+/**
+ * Module dependencies.
+ */
+
+import { default as isValidItin, mask } from '../src';
+import should from 'should';
+
+/**
+ * ITIN sample numbers.
+ */
+
+const invalidNumbers = [
+  '00070000',
+  '10070000',
+  '20070000',
+  '30070000',
+  '40070000',
+  '50070000',
+  '60070000',
+  '70070000',
+  '80070000',
+  '90000000',
+  '90010000',
+  '90020000',
+  '90030000',
+  '90040000',
+  '90050000',
+  '90060000',
+  '90089000',
+  '90089000',
+  '90093000'
+];
+const validNumbers = [
+  '900700000',
+  '900709999',
+  '999709999',
+  '900880000',
+  '900889999',
+  '999889999',
+  '900900000',
+  '900909999',
+  '999909999',
+  '900920000',
+  '900929999',
+  '999929999',
+  '900940000',
+  '900949999',
+  '999949999',
+  '900990000',
+  '900999999',
+  '999999999'
+];
+
+/**
+ * Test.
+ */
+
+describe('isValidItin', () => {
+  describe('default()', () => {
+    it('should return `false` if itin is invalid', () => {
+      for (const itin of invalidNumbers) {
+        isValidItin(itin).should.be.false();
+      }
+    });
+
+    it('should return `true` if itin is valid', () => {
+      for (const itin of validNumbers) {
+        isValidItin(itin).should.be.true();
+      }
+    });
+  });
+
+  describe('mask()', () => {
+    it('should mask a valid itin', () => {
+      mask('900709999').should.equal('XXXXX9999');
+    });
+
+    it('should throw an error if itin is invalid', () => {
+      try {
+        mask('00070000');
+
+        should.fail();
+      } catch (e) {
+        e.should.be.instanceOf(Error);
+        e.message.should.equal('Invalid Individual Taxpayer Identification Number');
+      }
+    });
+  });
+});
