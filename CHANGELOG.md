25ab5ce4320f4429d15fb2121feb540425adc521
Rui Marinho <rui@uphold.com>
2016-04-13 19:08:13 +0100

    Release 1.0.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,10 +1,16 @@
 # Changelog
 
-## [0.0.2](https://github.com/seegno/itin-validator/tree/0.0.2) (2016-02-17)
-[Full Changelog](https://github.com/seegno/itin-validator/compare/0.0.1...0.0.2)
+## [v1.0.0](https://github.com/seegno/itin-validator/tree/v1.0.0) (2016-04-13)
+[Full Changelog](https://github.com/seegno/itin-validator/compare/v0.0.2...v1.0.0)
+
+**Merged pull requests:**
+
+- Update algorithm to accept formatted and non formatted numbers [\#5](https://github.com/seegno/itin-validator/pull/5) ([franciscocardoso](https://github.com/franciscocardoso))
+
+## [v0.0.2](https://github.com/seegno/itin-validator/tree/v0.0.2) (2016-02-17)
+[Full Changelog](https://github.com/seegno/itin-validator/compare/v0.0.1...v0.0.2)
 
 **Merged pull requests:**
 
 - Update `es2015-node4` to `es2015` [\#3](https://github.com/seegno/itin-validator/pull/3) ([ruipenso](https://github.com/ruipenso))
 
-## [0.0.1](https://github.com/seegno/itin-validator/tree/0.0.1) (2016-02-05)
--- dist/src/index.js
+++ dist/src/index.js
@@ -3,11 +3,10 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
+exports.isValid = isValid;
 exports.mask = mask;
 
 /**
- * See `http://www.irs.gov/Individuals/General-ITIN-Information` for more information.
- *
  * An Individual Taxpayer Identification Number (ITIN) is a tax processing number issued by the Internal Revenue Service.
  * It is a nine-digit number that always begins with the number 9 and has a range of 70-88 in the fourth and fifth digit.
  * Effective April 12, 2011, the range was extended to include:
@@ -15,35 +14,35 @@ exports.mask = mask;
  *  - 900-70-0000 through 999-88-9999
  *  - 900-90-0000 through 999-92-9999
  *  - 900-94-0000 through 999-99-9999
+ *
+ * See `http://www.irs.gov/Individuals/General-ITIN-Information` for more information.
  */
 
-var expression = /^(9\d{2})((7[0-9]{1}|8[0-8]{1})|(9[0-2]{1})|(9[4-9]{1}))(\d{4})$/;
+/**
+ * Expression.
+ */
+
+var expression = /^(9\d{2})[- ]{0,1}((7[0-9]{1}|8[0-8]{1})|(9[0-2]{1})|(9[4-9]{1}))[- ]{0,1}(\d{4})$/;
 
 /**
  * Validate function.
  */
 
-function isValid(itin) {
-  return expression.test(itin);
+function isValid(value) {
+  return expression.test(value);
 }
 
 /**
- * Masks the ITIN with "X" placeholders to protect sensitive data,
+ * Mask the ITIN with "X" placeholders to protect sensitive data,
  * while keeping some of the original digits for contextual recognition.
  *
- * E.g. "123456789" -> "XXXXX6789"
+ * E.g. "123456789" -> "XXXXX6789", "123-45-6789" -> "XXX-XX-6789".
  */
 
-function mask(itin) {
-  if (!isValid(itin)) {
+function mask(value) {
+  if (!isValid(value)) {
     throw new Error('Invalid Individual Taxpayer Identification Number');
   }
 
-  return itin.substr(0, itin.length - 4).replace(/[\w]/g, 'X') + itin.substr(-4);
+  return '' + value.substr(0, value.length - 4).replace(/[\w]/g, 'X') + value.substr(-4);
 }
-
-/**
- * Export default.
- */
-
-exports.default = isValid;
